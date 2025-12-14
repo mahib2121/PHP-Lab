e523181b0bfa8156acbc12ae2a4ba1a48f4eb6db
mahib2121 <khanmahibalam@gmail.com>
2025-12-14 13:56:29 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+$length = 10;
+$width  = 5;
+
+
+$area = $length * $width;
+$perimeter = 2 * ($length + $width);
+
+echo($area);
+?>
+
+</body>
+</html>
